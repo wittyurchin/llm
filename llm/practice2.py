e1a96bba578ed1fa0ff4cafe9ac6af5f9759b641
purnachandra <purnachandra.patro@gmail.com>
2023-11-04 21:58:01 +0530

    llm code from deep learning.ai class 2

---
+++ llm/practice2.py
@@ -0,0 +1,55 @@
+import openai
+import os
+
+openai.api_key = ""
+
+def llm_response(prompt):
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{'role':'user','content':prompt}],
+        temperature=0
+    )
+    return response.choices[0].message['content']
+
+all_reviews = [
+    'The mochi is excellent!',
+    'Best soup dumplings I have ever eaten.',
+    'Not worth the 3 month wait for a reservation.',
+    'The colorful tablecloths made me smile!',
+    'The pasta was cold.'
+]
+
+all_reviews
+
+all_sentiments = []
+for review in all_reviews:
+    prompt = f'''
+        Classify the following review 
+        as having either a positive or
+        negative sentiment. State your answer
+        as a single word, either "positive" or
+        "negative":
+
+        {review}
+        '''
+    response = llm_response(prompt)
+    all_sentiments.append(response)
+
+all_sentiments
+
+
+all_sentiments = []
+for review in all_reviews:
+    prompt = f'''
+        Classify the following review 
+        as having either a positive or
+        negative sentiment. State your answer
+        as a single word, either "positive" or
+        "negative":
+
+        {review}
+        '''
+    response = llm_response(prompt)
+    all_sentiments.append(response)
+
+all_sentiments
